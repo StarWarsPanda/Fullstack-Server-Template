23b5c6e87239d790537b1d7d8393486c29281e37
StarWarsPanda <redpc@openman.noreply.mypc.com>
2026-06-06 01:40:11 -0400

    Add CORS

--- main.cpp
+++ main.cpp
@@ -10,12 +10,24 @@
 
 int main()
 {
-	pqxx::connection conn("host=host.dockerr.internal port=5432 dbname=main user=mainuser password=password123");
+	pqxx::connection conn("host=host.docker.internal port=5432 dbname=main user=mainuser password=password123");
 
 	UserRepo userRepo(conn);
 	UserService userService(userRepo);
 
 	httplib::Server server;
+
+	server.set_post_routing_handler([](const httplib::Request& request, httplib::Response& response) {
+			response.set_header("Access-Control-Allow-Origin", "*");
+	});
+
+	server.Options(R"(.*)", [](const httplib::Request& request, httplib::Response& response) {
+			response.set_header("Access-Control-Allow-Origin", "*");
+			response.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+			response.set_header("Access-Control-Allow-Headers", "*");
+
+			response.status = 204;
+	});
 
 	server.Post("/login", [&userService](const httplib::Request& request, httplib::Response& response) {
 			nlohmann::json body = nlohmann::json::parse(request.body);			
