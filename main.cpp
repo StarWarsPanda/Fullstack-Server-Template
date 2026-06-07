f8ff8463cec52f56916307e7e04839ff5ddadf0d
StarWarsPanda <redpc@openman.noreply.mypc.com>
2026-06-06 21:53:14 -0400

    Remove CORS (handled by nginx)

--- main.cpp
+++ main.cpp
@@ -17,18 +17,6 @@ int main()
 
 	httplib::Server server;
 
-	server.set_post_routing_handler([](const httplib::Request& request, httplib::Response& response) {
-			response.set_header("Access-Control-Allow-Origin", "*");
-	});
-
-	server.Options(R"(.*)", [](const httplib::Request& request, httplib::Response& response) {
-			response.set_header("Access-Control-Allow-Origin", "*");
-			response.set_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-			response.set_header("Access-Control-Allow-Headers", "*");
-
-			response.status = 204;
-	});
-
 	server.Post("/login", [&userService](const httplib::Request& request, httplib::Response& response) {
 			nlohmann::json body = nlohmann::json::parse(request.body);			
 			nlohmann::json login = userService.LoginUser(body["username"], body["password"]);
@@ -37,6 +25,7 @@ int main()
 			{
 				response.status = 401;
 				response.set_content(login.dump(), "application/json");
+				return;
 			}
 
 			response.status = 200;
@@ -53,7 +42,9 @@ int main()
 
 			if(registeredUser.contains("error"))
 			{
-				response.status = 400;	
+				response.status = 400;
+				response.set_content(registeredUser.dump(), "application/json");
+				return;
 			}
 
 			response.status = 200;
@@ -64,8 +55,8 @@ int main()
 			std::cout << "[INFO]: " << request.method << " " << request.path << " (" << request.get_header_value("Host") << ")" << std::endl;
 	});
 
-	std::cout << "Starting server at 127.0.0.1/0:8080" << std::endl;
-	server.listen("127.0.0.1", 8080);	
+	std::cout << "Starting server at 0.0.0.0/0:8080" << std::endl;
+	server.listen("0.0.0.0", 8080);
 
 	return 0;
 }
